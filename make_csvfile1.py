320a71adf69fb1e07445a9f2ecb540c870ea12e5
ozikot <dendai0122@gmail.com>
2018-11-12 15:45:47 +0900

    CSV書き込み時にindexを含まない設定を追加

--- make_csvfile1.py
+++ make_csvfile1.py
@@ -1,33 +1,24 @@
-# -*- coding: utf-8 -*-
-
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# data size
+# 生成するデータの行数
 ds = 30000
 
-"""
-# Generate heartbeat data with simple random walk
-# initial value(p0) = 100
-dn = np.random.choice([-1, 1], size=ds)
-p0 = 100
-swalk = np.cumsum(dn) + p0
-"""
 
-# Generate heartbeat data with geometric random walk
-# initial value(p0) = 100
+# 幾何ランダムウォークで疑似的な心拍データの生成
 dn = np.random.choice([-1, 1], size=ds)
 p0 = 100
 
 gwalk = np.cumprod(np.exp(dn * 0.01)) * p0
 pd.Series(gwalk).plot()
-plt.show()
 
-# Generate data frame
+
+# データフレームを生成
 df = pd.DataFrame({
     'Time' : pd.date_range('2018/09/01 00:00:00', periods=ds, freq='1S'),
-    'bpm' : pd.Series(gwalk)
+    'BPM' : pd.Series(gwalk)
 })
 
-df.to_csv('test.csv')
+# データフレームをCSVファイル形式で保存
+df.to_csv('heartbeat.csv', index=None)
