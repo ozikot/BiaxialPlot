707509770eb7eba832c98f4cd67a6410da4d9c21
ozikot <dendai0122@gmail.com>
2018-11-13 17:00:03 +0900

    パスを変更

--- ep2_merge/merge.py
+++ ep2_merge/merge.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # 心拍と体表温度のCSVファイルを読み込む
-df_hb = pd.read_csv('..//heartbeat.csv')
-df_tm = pd.read_csv('..//temp_bs.csv')
+df_hb = pd.read_csv('..//csv_file//heartbeat.csv')
+df_tm = pd.read_csv('..//temp_bs_edited.csv')
 
 print(df_tm.head())
